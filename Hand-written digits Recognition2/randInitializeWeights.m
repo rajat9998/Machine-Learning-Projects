93bd83dfc5658e9e3fa2aedd0f9c47d9071a26ed
Rajat Kumar <rajat.kumar9998@gmail.com>
2019-09-01 00:10:48 +0530

    Neural Networks

--- Hand-written digits Recognition2/randInitializeWeights.m
+++ Hand-written digits Recognition2/randInitializeWeights.m
@@ -3,10 +3,9 @@ function W = randInitializeWeights(L_in, L_out)
 %   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
 %   of a layer with L_in incoming connections and L_out outgoing 
 %   connections. 
-%
+
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
 %   the first column of W handles the "bias" terms
-%
 
 % Return the following variables correctly 
 W = zeros(L_out, 1 + L_in);
