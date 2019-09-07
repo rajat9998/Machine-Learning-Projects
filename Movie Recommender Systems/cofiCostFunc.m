64dd97fc6ed97421b16e530a0bb29ce4deab1607
Rajat Kumar <rajat.kumar9998@gmail.com>
2019-09-07 18:41:54 +0530

    Anomaly Detection & Recommender Systems

---
+++ Movie Recommender Systems/cofiCostFunc.m
@@ -0,0 +1,42 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
+
+
+% Unfold the U and W matrices from params
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+            
+% Return the following values correctly
+J = 0;
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+
+
+%%%%%% Without Regularization %%%%%%%%%%
+  Error = (X * Theta') - Y;
+  
+  J = 1/2 * sum(sum(Error .^2 .*R));
+  
+  X_grad = (Error .* R) * Theta;   % Nm x n
+  Theta_grad = (Error .* R)' * X;  % Nu x n
+  
+%%%%%%%%% With Regularization %%%%%%%%%%
+  Reg_term_theta = lambda/2 * sum(sum(Theta .^2));
+  Reg_term_x = lambda/2 * sum(sum(X .^2));
+  
+  J = J + Reg_term_theta + Reg_term_x;
+  
+  X_grad = X_grad + lambda * X;             % Nm x n
+  Theta_grad = Theta_grad + lambda * Theta; % Nu x n
+
+
+grad = [X_grad(:); Theta_grad(:)];
+
+end
